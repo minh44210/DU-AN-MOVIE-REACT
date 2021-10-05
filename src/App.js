ad142b4acdc7eb36b443c7b46450d4916f11a969
Kminh <minh44210@gmail.com>
2021-10-05 16:04:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutPage from "./containers/HomeTemplete/AboutPage";
+import HomePage from "./containers/HomeTemplete/HomePage";
+import ListMoviePage from "./containers/HomeTemplete/ListMoviePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PageNotFound from "./containers/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      {/* Trang chủ */}
+      <Route exact path="/" component={HomePage} />
+      {/* About */}
+      <Route path="/about" component={AboutPage} />
+      {/* Moive List */}
+      <Route path="/movie-list" component={ListMoviePage} />
+      {/* Trang không tồn tại  */}
+      <Route path="" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/containers/PageNotFound/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./style.css";
+export default class PageNotFound extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="site">
+            <div className="sketch">
+              <div className="bee-sketch red" />
+              <div className="bee-sketch blue" />
+            </div>
+            <h1>
+              404:
+              <small>Page not found</small>
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
