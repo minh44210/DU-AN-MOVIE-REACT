f41860cbe966cbe2162b99aaed90aadc7197297f
disaster253 <65069829+disaster253@users.noreply.github.com>
2021-10-13 23:01:24 +0700

    Update App.js
    edit src

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PageNotFound from "./containers/PageNotFound";
-<<<<<<< HEAD
 import { renderRoutesHome, renderRoutesAdmin } from "./routes";
-// import Footer from "./containers/HomeTemplete/_component/footer";
-=======
-import { renderRoutesHome } from "./routes";
-import Footer from "./containers/HomeTemplete/_component/footer";
->>>>>>> 743211a6bdd33b316b5711b31e3b245b8cff3fdb
 import "antd/dist/antd.css";
 
 function App() {
-<<<<<<< HEAD
 	return (
 		<BrowserRouter>
 			<Switch>
@@ -26,27 +19,6 @@ function App() {
 				{/* Trang không tồn tại  */}
 				<Route path="" component={PageNotFound} />
 			</Switch>
-
 		</BrowserRouter>
 	);
-=======
-  return (
-    <BrowserRouter>
-      <Switch>
-        {renderRoutesHome()}
-        {/* Trang chủ */}
-        {/* <Route exact path="/" component={HomePage} /> */}
-        {/* About */}
-        {/* <Route path="/about" component={AboutPage} /> */}
-        {/* Moive List */}
-        {/* <Route path="/movie-list" component={ListMoviePage} /> */}
-        {/* Trang không tồn tại  */}
-        <Route path="" component={PageNotFound} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
->>>>>>> 743211a6bdd33b316b5711b31e3b245b8cff3fdb
-}
-
 export default App;
