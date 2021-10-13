4d6a677ca9126757d8914827fa1ae3e7c9372ee4
disaster253 <65069829+disaster253@users.noreply.github.com>
2021-10-13 22:59:41 +0700

    Update index.js
    edit src

--- src/containers/HomeTemplete/index.js
+++ src/containers/HomeTemplete/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Navbar from "./_component/Navbar";
-<<<<<<< HEAD
 // import PageNotFound from "../PageNotFound/index";
 import Footer from "./_component/footer";
 export default function HomeTemplate(props) {
@@ -12,28 +11,7 @@ export default function HomeTemplate(props) {
 			<Navbar />
 
 			<Route exact={exact} path={path} component={component} />
-			<Footer />
+			<Footer />		
 		</>
 	);
-=======
-import PageNotFound from "../PageNotFound/index";
-
-export default function HomeTemplate(props) {
-  const { exact, path, component } = props;
-
-  function LayoutHome(props) {
-    return (
-      <>
-        <Navbar />
-        {props.children}
-      </>
-    );
-  }
-
-  return (
-    <LayoutHome>
-      <Route exact={exact} path={path} component={component} />
-    </LayoutHome>
-  );
->>>>>>> 743211a6bdd33b316b5711b31e3b245b8cff3fdb
 }
